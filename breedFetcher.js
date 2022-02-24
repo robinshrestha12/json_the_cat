a82faa010a34c8d9a1432caf2dc9a6dc2783ac78
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-02-23 21:44:30 -0500

    Eslinted the file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,19 @@
 //importing the request
 const request = require('request');
 //getting input
-const breedName=process.argv[2];
-let link=`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`; //getting the URL
+const breedName = process.argv[2];
+let link = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`; //getting the URL
 
 request(link, (error, response, body) =>{
 
-  if(error){ //if URL does not work
+  if (error) { //if URL does not work
     console.log('Could not fetch URL ', error);
     return;
   }
   const data = JSON.parse(body); //converting JSON string to an actual object
-  if(data.length===0){ //if data is empty, breed is not found.
+  if (data.length === 0) { //if data is empty, breed is not found.
     console.log("Breed name is not found");
     return;
   }
-   console.log(data[0]['description']);
-  }); 
+  console.log(data[0]['description']);
+});
